aa424fe8c6cd83a892faf906535e94ebc81c7cfe
gisellitiWeb <oreodemorango.contato@gmail.com>
2024-01-15 13:19:08 -0300

    testes realizados!

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TestesUnitarios.Desafio.Console.Services;
+
+ValidacoesLista li = new ValidacoesLista();
+
+List <int> Numeros = new List<int>();
+Numeros.Add(9);
+Numeros.Add(7);
+Numeros.Add(3);
+Numeros.Add(6);
+li.MultiplicarNumerosLista(Numeros, 2);
